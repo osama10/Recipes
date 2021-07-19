b5d5d5bd1ad4fb9a8da7d700dc36cf6f35279479
Osama Bin Bashir <osama.bashir@hellofresh.com>
2021-07-20 01:40:38 +0200

    add details

---
+++ MVVMC Example/App/UI/Recipe/RecipeDetails/View/RecipeDetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RecipeDetailsViewController.swift
+//  MVVMC Example
+//
+//  Created by Osama Bashir on 20.07.21.
+//
+
+import UIKit
+
+final class RecipeDetailsViewController: UIViewController {
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var recipeTitle: UILabel!
+    @IBOutlet weak var recipeHeadline: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
