bc500ec5fcaa523f6acaffd6186fb1a436ea8a38
abbas-a-awan <aaa@hellofresh.com>
2021-07-20 14:10:28 +0200

    Added view model for recipes list

--- MVVMC Example/App/UI/Recipe/RecipeList/ViewModel/RecipeListViewModel.swift
+++ MVVMC Example/App/UI/Recipe/RecipeList/ViewModel/RecipeListViewModel.swift
@@ -6,4 +6,81 @@
 //
 
 import Foundation
+import Combine
+
+enum ViewState {
+    case initial
+    case loading
+    case loaded
+    case failed(String?)
+}
+
+/// Receiving the input/event coming from the associated view
+/// It's better to keep the naming consistent with the view
+protocol RecipeListViewModelInput {
+    func viewDidLoad()
+    func didTapRow(at indexPath: IndexPath)
+}
+
+protocol RecipeListViewModelOutput {
+    var numberOfRows: Int { get }
+    var viewStateUpdated: ((ViewState) -> Void)? { get set }
+
+    func dateCellViewModel() -> DateCellViewModel
+    func recipeCellViewModel(at indexPath: IndexPath) -> RecipeCellViewModel
+}
+
+protocol RecipeListViewModelProtocol: RecipeListViewModelInput, RecipeListViewModelOutput {
+
+}
+
+final class RecipeListViewModel: RecipeListViewModelProtocol {
+    private let recipeUseCase: RecipeUseCaseProtocol
+    private var recipes = [Recipe]()
+    private var cancelSubscription = Set<AnyCancellable>()
+
+    var viewStateUpdated: ((ViewState) -> Void)?
+
+    init(recipeUseCase: RecipeUseCaseProtocol) {
+        self.recipeUseCase = recipeUseCase
+    }
+}
+
+// MARK: - Input protocol
+extension RecipeListViewModel {
+    func viewDidLoad() {
+
+        viewStateUpdated?(.loading)
+        recipeUseCase.getRecipes().receive(on: RunLoop.main).sink { result in
+
+            switch result {
+            case .failure(let error):
+                self.viewStateUpdated?(.failed(error.localizedDescription))
+            case .finished:
+                self.viewStateUpdated?(.loaded)
+            }
+        } receiveValue: { recipes in
+            self.recipes = recipes
+        }.store(in: &cancelSubscription)
+    }
+    
+    func didTapRow(at indexPath: IndexPath) {
+
+    }
+}
+
+// MARK: - Output protocol
+extension RecipeListViewModel {
+    var numberOfRows: Int {
+        recipes.count + 1
+    }
+
+    func dateCellViewModel() -> DateCellViewModel {
+        DateCellViewModel()
+    }
+
+    func recipeCellViewModel(at indexPath: IndexPath) -> RecipeCellViewModel {
+        RecipeCellViewModel(recipe: recipes[indexPath.row - 1])
+    }
+}
 
